e3e1aeeef78ed55203f72dc00dd8df60a7c29942
Michael Bitzi <mibitzi@gmail.com>
2016-04-27 23:23:38 +0200

    models

--- weiqi/models.py
+++ weiqi/models.py
@@ -14,8 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.orm import validates
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import validates, relationship
 from datetime import datetime
 import re
 import bcrypt
@@ -36,6 +36,9 @@ class User(Base):
     display = Column(String, nullable=False, default='')
     rating = Column(Float, nullable=False, default=0)
 
+    rooms = relationship('RoomUser', back_populates='user')
+    messages = relationship('RoomMessage', back_populates='user')
+
     def set_password(self, pw):
         self.password = bcrypt.hashpw(pw.encode(), bcrypt.gensalt()).decode()
 
@@ -53,3 +56,47 @@ class User(Base):
         if not re.match(r'^[a-zA-Z0-9_-]{2,12}$', val):
             raise ValueError('invalid display name')
         return val
+
+
+class Room(Base):
+    __tablename__ = 'rooms'
+
+    id = Column(Integer, primary_key=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    name = Column(String, nullable=False, default='Room')
+
+    users = relationship('RoomUser', back_populates='room')
+    messages = relationship('RoomMessage', back_populates='room')
+
+
+class RoomUser(Base):
+    __tablename__ = 'room_users'
+
+    room_id = Column(ForeignKey('rooms.id'), primary_key=True)
+    user_id = Column(ForeignKey('users.id'), primary_key=True)
+
+    room = relationship('Room', back_populates='users')
+    user = relationship('User', back_populates='rooms')
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    has_unread = Column(Boolean, nullable=False, default=False)
+
+
+class RoomMessage(Base):
+    __tablename__ = 'room_messages'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    room_id = Column(ForeignKey('rooms.id'), nullable=False)
+    room = relationship('Room', back_populates='messages')
+
+    user_id = Column(ForeignKey('users.id'), nullable=False)
+    user = relationship('User', back_populates='messages')
+
+    message = Column(String, nullable=False)
