962396b66c5b413d7f29216e7979d826501e657b
Michael Bitzi <mibitzi@gmail.com>
2016-05-22 16:41:26 +0200

    reduce deployment downtime

--- deploy.sh
+++ deploy.sh
@@ -14,15 +14,20 @@ echo "Deploying weiqi.gs ..."
 
 /usr/bin/ssh -t ${HOST} <<EOF
 cd ${PATH}
-sudo supervisorctl stop weiqi
 git fetch --all
 git reset --hard origin/master
+
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade -r requirements.txt
 npm install
+
 node_modules/.bin/gulp build --production
+
+sudo supervisorctl stop weiqi
+
 export WEIQI_SETTINGS=\$HOME/settings.py
 alembic upgrade head
+
 sudo supervisorctl start weiqi
 EOF
