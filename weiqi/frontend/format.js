b7b590ecf3c9c827f8d61f0220108f8f6d771d09
Michael Bitzi <mibitzi@gmail.com>
2016-05-22 15:09:29 +0200

    don't show minus durations

--- weiqi/frontend/format.js
+++ weiqi/frontend/format.js
@@ -5,8 +5,12 @@ export function pad(n) {
 export function format_duration(seconds) {
     var sec = Math.ceil(seconds);
     var min = Math.floor(sec / 60);
+    
+    if(sec <= 0) {
+        return '00:00';
+    }
 
     sec -= min * 60;
-
+    
     return pad(Math.round(min)) + ":" + pad(Math.round(sec));
 }
