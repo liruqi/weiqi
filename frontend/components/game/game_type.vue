413f620195654abdf3aac47a45cd2d117559b821
Michael Bitzi <mibitzi@gmail.com>
2016-06-12 11:19:48 +0200

    fix game types for ranked/free

--- frontend/components/game/game_type.vue
+++ frontend/components/game/game_type.vue
@@ -3,8 +3,10 @@
         <template v-if="game.is_demo">{{$t('game.demo')}}</template>
         <template v-else>
             <template v-if="game.is_ranked">{{$t('game.ranked')}}</template>
-            <template v-if="game.is_private">{{$t('game.private')}}</template>
-            <template v-else>{{$t('game.free')}}</template>
+            <template v-else>
+                <template v-if="game.is_private">{{$t('game.private')}}</template>
+                <template v-else>{{$t('game.free')}}</template>
+            </template>
         </template>
     </span>
 </template>
