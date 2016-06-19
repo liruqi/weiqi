e536866c547aca31a0d3e939cc311a6807af3512
Michael Bitzi <mibitzi@gmail.com>
2016-06-19 13:48:32 +0200

    prefix prometheus metrics

--- weiqi/metrics.py
+++ weiqi/metrics.py
@@ -17,8 +17,8 @@
 from prometheus_client import Summary, Gauge, Counter
 
 
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing requests', ['method'])
-SENT_MESSAGES = Summary('sent_message_bytes', 'Size of outgoing messages', ['method'])
-CONNECTED_SOCKETS = Gauge('connected_sockets', 'Number of connected websockets')
-EXCEPTIONS = Counter('exceptions_total', 'Number of exceptions in requests', ['method'])
-REGISTRATIONS = Counter('registrations_total', 'Total number of registrations')
+REQUEST_TIME = Summary('weiqi_request_processing_seconds', 'Time spent processing requests', ['method'])
+SENT_MESSAGES = Summary('weiqi_sent_message_bytes', 'Size of outgoing messages', ['method'])
+CONNECTED_SOCKETS = Gauge('weiqi_connected_sockets', 'Number of connected websockets')
+EXCEPTIONS = Counter('weiqi_exceptions_total', 'Number of exceptions in requests', ['method'])
+REGISTRATIONS = Counter('weiqi_registrations_total', 'Total number of registrations')
