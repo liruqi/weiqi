88c35a4c830f40a6fcb2f1bc3837508f0d269807
Michael Bitzi <mibitzi@gmail.com>
2016-06-28 11:21:28 +0200

    WIP test mithril

--- frontend/components/room/room_logs.js
+++ frontend/components/room/room_logs.js
@@ -1,23 +1,33 @@
 import m from 'mithril';
 
 export function controller() {
+    var ctrl = this;
+    
+    ctrl.message = m.prop('');
+    
+    ctrl.submit = function() {
+        console.log('submit', ctrl.message());
+        ctrl.message('');
+        return false;
+    }
 }
 
-export function view(ctrl) {
-    var logs = [];
-    var room = {};
+export function view(ctrl, args) {
+    var room = args.state.rooms.find(function(room) { return room.id == args.room_id(); });
+    var logs = args.state.room_logs[args.room_id()];
+    var $t = function() {};
     
     return (
         m('.panel.panel-default.flex-column.room-logs.fixed-dropdowns', [
             m('.panel-heading.flex-fixed',
                 m('h3.panel-title', [
                     m('i.fa.fa-comments-o'),
-                    m('span', (args.title ? args.title : (room.type=='main' ? room.name : $t('room_logs.chat')))),
+                    m('span', (args.title ? args.title : (room.type=='main' ? room.name : $t('room_logs.chat'))))
                 ])
             ),
             m('.panel-body.flex-auto', logs.map(function(log) {
                 return [
-                    m('img.avatar[src=/api/users/' + log.user_id + '/avatar'),
+                    m('img.avatar[src=/api/users/' + log.user_id + '/avatar]'),
                     m('p.message', [
                         m('span.name', [
                             log.user_display,
@@ -30,9 +40,9 @@ export function view(ctrl) {
                 ]
             })),
             m('.panel-footer.flex-fixed',
-                m('form',
+                m('form', {onsubmit: ctrl.submit},
                     m('.input-group', [
-                        m('input.form-control[type=text,name=message,autocomplete=off]'),
+                        m('input.form-control[type=text][name=message][autocomplete=off]', {onchange: m.withAttr('value', ctrl.message), value: ctrl.message()}),
                         m('.input-group-btn',
                             m('button.btn.btn-success',
                                 m('i.fa.fa-plus'))
